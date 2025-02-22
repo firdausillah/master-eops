3efe18624ed57efde8dbe7cbf4f5156f673ddfa7
firdausillah <firdausillah123@gmail.com>
2025-02-22 10:13:15 +0700

    update impor

--- application/controllers/admin/Import_excel.php
+++ application/controllers/admin/Import_excel.php
@@ -22,10 +22,6 @@ class Import_excel extends CI_Controller
 
     public function import_data_siswa()
     {
-        if (!class_exists('PhpOffice\PhpSpreadsheet\Reader\Xlsx')) {
-            die('Class Xlsx not found. Pastikan PhpSpreadsheet sudah terinstall dengan benar.');
-        }
-
         $file_mimes = array('text/x-comma-separated-values', 'text/comma-separated-values', 'application/octet-stream', 'application/vnd.ms-excel', 'application/x-csv', 'text/x-csv', 'text/csv', 'application/csv', 'application/excel', 'application/vnd.msexcel', 'text/plain', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
         if (isset($_FILES['excel']['name']) && in_array($_FILES['excel']['type'], $file_mimes)) {
             $arr_file = explode('.', $_FILES['excel']['name']);
